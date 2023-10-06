cad5bb4ed157412f395eb918be10e54263a5d46b
cesar <haliercesr@gmail.com>
2023-10-06 20:57:33 -0300

    deploy

--- src/components/redux/actions/actions.js
+++ src/components/redux/actions/actions.js
@@ -39,7 +39,7 @@ export const searchDogs = (raza) => {
         try {
           
             const { data } = await axios.get(`${URL}/dogs/name?name=${raza}`)
-            const data1=filtrarDogsCreados(data) //filtro los datos del server para los perros creados en la BBD, TENGO QUE HACERLO EN EL SERVER Y NO EN EL FONTEND
+            const data1=data===false?[]:filtrarDogsCreados(data) //filtro los datos del server para los perros creados en la BBD, TENGO QUE HACERLO EN EL SERVER Y NO EN EL FONTEND
             return dispatch(
                 {
                     type: SEARCH_DOGS,
